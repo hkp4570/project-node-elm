7ce92f8a73accb92467939d19c26543aaab94d66
hkp <colourful4570@163.com>
2024-07-17 14:14:34 +0800

    修改密码

--- controller/v2/user.js
+++ controller/v2/user.js
@@ -9,11 +9,12 @@ class UserHandler extends AddressComponent {
   constructor() {
     super();
     this.login = this.login.bind(this);
+    this.changePassword = this.changePassword.bind(this);
   }
 
   async login(req, res) {
     const cap = req.cookies.cap;
-    if(!cap){
+    if (!cap) {
       console.log('验证码失效')
       res.send({
         status: 0,
@@ -94,6 +95,85 @@ class UserHandler extends AddressComponent {
   md5(password) {
     const md5 = crypto.createHash('md5');
     return md5.update(password).digest('base64');
+  }
+
+  async changePassword(req, res) {
+    const cap = req.cookies.cap;
+    if (!cap) {
+      console.log('验证码失效')
+      res.send({
+        status: 0,
+        type: 'ERROR_CAPTCHA',
+        message: '验证码失效',
+      })
+      return
+    }
+    const form = formidable({});
+    form.parse(req, async (err, fields, files) => {
+      const {username, oldPassWord, newPassword, confirmPassword, captcha_code} = fields;
+      try {
+        if (!username) {
+          throw new Error('用户名参数错误');
+        } else if (!oldPassWord) {
+          throw new Error('旧密码参数错误');
+        } else if (!newPassword) {
+          throw new Error('新密码参数错误');
+        } else if (!confirmPassword) {
+          throw new Error('确认密码参数错误');
+        } else if (newPassword !== confirmPassword){
+          throw new Error('新密码与确认密码不一致');
+        } else if (!captcha_code){
+          throw new Error('验证码错误');
+        }
+      } catch (err) {
+        console.log('修改密码参数错误', err);
+        res.send({
+          status: 0,
+          type: 'ERROR_QUERY',
+          message: err.message,
+        })
+        return
+      }
+      if(cap.toUpperCase().toString() !== captcha_code.toUpperCase().toString()){
+        res.send({
+          status: 0,
+          type: 'ERROR_CAPTCHA',
+          message: '验证码不正确',
+        })
+        return
+      }
+      const md5Password = this.encryption(oldPassWord);
+      try{
+        const user = await UserModel.findOne({username});
+        if(!user){
+          res.send({
+            status: 0,
+            type: 'USER_NOT_FOUND',
+            message: '未找到当前用户',
+          })
+        }else if (user.password.toString() !== md5Password.toString()){
+          res.send({
+            status: 0,
+            type: 'ERROR_PASSWORD',
+            message: '密码不正确',
+          })
+        } else {
+          user.password = this.encryption(newPassword);
+          user.save();
+          res.send({
+            status: 1,
+            success: '密码修改成功',
+          })
+        }
+      }catch (err) {
+        console.log('修改密码失败', err);
+        res.send({
+          status: 0,
+          type: 'ERROR_CHANGE_PASSWORD',
+          message: '修改密码失败',
+        })
+      }
+    })
   }
 }
 
--- routes/v2.js
+++ routes/v2.js
@@ -5,5 +5,6 @@ import UserHandler from '../controller/v2/user';
 const router = express.Router();
 
 router.post('/login', UserHandler.login);
+router.post('/changepassword', UserHandler.changePassword);
 
 export default router;
